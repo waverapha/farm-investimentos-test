bb8e90e868c67497d0c9a27b40e3c790af82893e
Rapha <raphael.v.work@gmail.com>
2023-03-14 16:29:25 -0300

    feat(ts): add country type

---
+++ src/types/country.ts
@@ -0,0 +1,13 @@
+export interface Country {
+  ID: string,
+  Country: string,
+  CountryCode: string,
+  Slug: string,
+  NewConfirmed: number,
+  TotalConfirmed: number,
+  NewDeaths: number,
+  TotalDeaths: number,
+  NewRecovered: number,
+  TotalRecovered: number,
+  Date: string
+}
